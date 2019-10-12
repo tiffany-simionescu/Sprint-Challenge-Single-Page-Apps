a01ea51060ba5de87f995baa8318b80ac7bc9af1
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-11 21:24:11 -0400

    Characters, added react-router-dom, axios

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import {Card, CardBody, CardImg, CardText} from "reactstrap";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <Card>
+      <CardBody>
+        <CardImg src={props.image} />
+        <CardText>Name: {props.name}</CardText>
+        <CardText>Status: {props.status}</CardText>
+        <CardText>Species: {props.species}</CardText>
+        <CardText>Gender: {props.gender}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
